a5789a6e21b7fe89573c90f67205f6a231079658
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2022-02-24 02:56:26 -0700

    Study Area MCPs

--- 2.Chapter1/1.Scripts/UsedToAvailable_Analysis.R
+++ 2.Chapter1/1.Scripts/UsedToAvailable_Analysis.R
@@ -14,6 +14,7 @@ library(sf)
 library(raster)
 library(tidyverse)
 library(adehabitatHR)
+library(mapview)
 #      Functions                                                            ####
 
 #      Data                                                                 ####
@@ -43,12 +44,63 @@ mcp_deer_north <- mcp(xy = spatialpoints_deer_north,
                       unin = "m",
                       unout = "km2")
 
+# Masking and Cropping
+
+available_north_NLCD <- mask(NLCD_Missouri,mcp_deer_north) %>% crop(mcp_deer_north)
+
+# Visual Inspection 
+
+mapview(available_north_NLCD)
+
 #      [South]                                                              ####
 
 # Subsetting only South locations
-df_deer_sorth <- df_deer %>% filter(site == "South")
+df_deer_south <- df_deer %>% 
+  filter(site == "South")
 
-#      [Southeast]                                                     ####
+# Making a SpatialPoints Object for MCP
+spatialpoints_deer_south <-SpatialPoints(coords = cbind(df_deer_south$x,df_deer_south$y),
+                                         proj4string = crs(NLCD_Missouri))
+
+# MCP 
+mcp_deer_south <- mcp(xy = spatialpoints_deer_south,
+                      percent = 95,
+                      unin = "m",
+                      unout = "km2")
+
+# Masking and Cropping
+
+available_south_NLCD<- mask(NLCD_Missouri,mcp_deer_south) %>% 
+  crop(mcp_deer_south)
+
+# Visual Inspection 
+
+#mapview(available_south_NLCD)
+
+#      [Southeast]                                                          ####
 
 # Subsetting only Southeast locations
-df_deer_southeast <- df_deer %>% filter(site == "Southeast")
+df_deer_southeast <- df_deer %>% 
+  filter(site == "Southeast")
+
+# Making a SpatialPoints Object for MCP
+spatialpoints_deer_southeast <-SpatialPoints(coords = cbind(df_deer_southeast$x,df_deer_southeast$y),
+                                         proj4string = crs(NLCD_Missouri))
+
+# MCP 
+mcp_deer_southeast <- mcp(xy = spatialpoints_deer_southeast,
+                      percent = 95,
+                      unin = "m",
+                      unout = "km2")
+
+# Masking and Cropping
+
+available_southeast_NLCD<- mask(NLCD_Missouri,mcp_deer_southeast) %>% 
+  crop(mcp_deer_southeast)
+
+# Visual Inspection 
+
+#mapview(available_southeast_NLCD)
+
+###############################################################################
+
