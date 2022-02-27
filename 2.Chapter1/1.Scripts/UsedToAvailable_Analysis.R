b56264807020fefff8c8bda2efed0e854708e360
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2022-02-26 21:58:27 -0700

    Preparing Sim

--- 2.Chapter1/1.Scripts/UsedToAvailable_Analysis.R
+++ 2.Chapter1/1.Scripts/UsedToAvailable_Analysis.R
@@ -15,6 +15,7 @@ library(raster)
 library(tidyverse)
 library(adehabitatHR)
 library(mapview)
+
 #      Functions                                                            ####
 
 #      Data                                                                 ####
@@ -25,32 +26,38 @@ df_deer <- read_csv("1.DataManagement/CleanData/deer_all_clean.csv")
 #        [NLCD]                                                             ####
 
 NLCD_Missouri <- raster("1.DataManagement\\CleanData\\NLCD_Missouri.tif")
+NLCD_available_north <- raster("1.DataManagement\\CleanData\\NLCD_available_north.tif")
+NLCD_available_south <- raster("1.DataManagement\\CleanData\\NLCD_available_south.tif")
+NLCD_available_southeast <- raster("1.DataManagement\\CleanData\\NLCD_available_southeast.tif")
+
+#        [DEM and derivative layers]                                        ####
+
+Topo_Missouri <- stackOpen("1.DataManagement/CleanData/Topo_Missouri.stk")
+
+Topo_available_north <- stack(lapply( c("1.DataManagement/CleanData/Topo_available_north_DEM_Missouri.tif",
+                                        "1.DataManagement/CleanData/Topo_available_north_DEM_Slope.tif",
+                                        "1.DataManagement/CleanData/Topo_available_north_DEM_Aspect.tif",
+                                        "1.DataManagement/CleanData/Topo_available_north_DEM_TRI.tif"),
+                                      raster))
+
+names(Topo_available_north) <- c("North_Elevation",
+                                 "North_Slope",
+                                 "North_Aspect",
+                                 "North_Elevation")
+
+Topo_available_south <- raster("1.DataManagement/CleanData/Topo_available_south.tif")
+Topo_available_southeast <- raster("1.DataManagement/CleanData/Topo_available_southeast.tif")
 
 ###############################################################################
-#   [Delineating Study Area MCPs]                                           ####
+#   [Making spatial objects]                                                ####
 #      [North]                                                              ####
-
-# Subsetting only North locations
+# Subsetting only South locations
 df_deer_north <- df_deer %>% 
   filter(site == "North")
-  
-# Making a SpatialPoints Object for MCP
+
+# Making SpatialPoints Object
 spatialpoints_deer_north <-SpatialPoints(coords = cbind(df_deer_north$x,df_deer_north$y),
                                          proj4string = crs(NLCD_Missouri))
-
-# MCP 
-mcp_deer_north <- mcp(xy = spatialpoints_deer_north,
-                      percent = 95,
-                      unin = "m",
-                      unout = "km2")
-
-# Masking and Cropping
-
-available_north_NLCD <- mask(NLCD_Missouri,mcp_deer_north) %>% crop(mcp_deer_north)
-
-# Visual Inspection 
-
-mapview(available_north_NLCD)
 
 #      [South]                                                              ####
 
@@ -58,53 +65,33 @@ mapview(available_north_NLCD)
 df_deer_south <- df_deer %>% 
   filter(site == "South")
 
-# Making a SpatialPoints Object for MCP
+# Making SpatialPoints Object
 spatialpoints_deer_south <-SpatialPoints(coords = cbind(df_deer_south$x,df_deer_south$y),
                                          proj4string = crs(NLCD_Missouri))
 
-# MCP 
-mcp_deer_south <- mcp(xy = spatialpoints_deer_south,
-                      percent = 95,
-                      unin = "m",
-                      unout = "km2")
-
-# Masking and Cropping
-
-available_south_NLCD<- mask(NLCD_Missouri,mcp_deer_south) %>% 
-  crop(mcp_deer_south)
-
-# Visual Inspection 
-
-#mapview(available_south_NLCD)
 
 #      [Southeast]                                                          ####
 
-# Subsetting only Southeast locations
+# Subsetting only South locations
 df_deer_southeast <- df_deer %>% 
   filter(site == "Southeast")
 
-# Making a SpatialPoints Object for MCP
+# Making SpatialPoints Object
 spatialpoints_deer_southeast <-SpatialPoints(coords = cbind(df_deer_southeast$x,df_deer_southeast$y),
                                          proj4string = crs(NLCD_Missouri))
 
-# MCP 
-mcp_deer_southeast <- mcp(xy = spatialpoints_deer_southeast,
-                      percent = 95,
-                      unin = "m",
-                      unout = "km2")
-
-# Masking and Cropping
-
-available_southeast_NLCD<- mask(NLCD_Missouri,mcp_deer_southeast) %>% 
-  crop(mcp_deer_southeast)
-
-# Visual Inspection 
-
-#mapview(available_southeast_NLCD)
+###############################################################################
+#   [Sampling Used Locations]                                               ####
+#      [North - All]                                                        ####
 
 ###############################################################################
+#   [Sampling Available Locations]                                          ####
 
-#### Generating Available Points
+#      [Example]                                                            ####
 
 # Sample Code
-samp2011 <- sampleRandom(NLCD_Missouri, size = 20, na.rm = TRUE, sp = TRUE)
+samp <- sampleRandom(NLCD_available_north, size = 2000, na.rm = TRUE, sp = TRUE)
+
+mapview(NLCD_available_north)+mapview(samp)
+
+samp %>% as.data.frame()
