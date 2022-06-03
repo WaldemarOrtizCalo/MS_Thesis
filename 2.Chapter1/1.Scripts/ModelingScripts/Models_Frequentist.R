830f3acef18732faf9212dc3ceebfa880292601b
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2022-06-02 20:09:58 -0600

    Renaming File

--- 2.Chapter1/1.Scripts/ModelingScripts/Models_Frequentist.R
+++ 2.Chapter1/1.Scripts/ModelingScripts/Models_Frequentist.R
@@ -57,7 +57,7 @@ data_south <- read_csv("1.DataManagement/CleanData/Chapter1_FinalData/South_fina
 
 data_southeast <- read_csv("1.DataManagement/CleanData/Chapter1_FinalData/Southeast_final.csv") %>% 
   rename(choice = location_type,
-    proportion_water = Southeast_proportion_1_buffer600m,
+         proportion_water = Southeast_proportion_1_buffer600m,
          proportion_developed = Southeast_proportion_2_buffer600m,
          proportion_barren = Southeast_proportion_3_buffer600m,
          proportion_decidousforest = Southeast_proportion_4_buffer600m,
@@ -70,7 +70,13 @@ data_southeast <- read_csv("1.DataManagement/CleanData/Chapter1_FinalData/Southe
          contagion = Southeast_contag,
          landscapeshapeindex = Southeast_lsi,
          meanshapeindex = Southeast_meanshapeindex,
-         ShannonDiversityIndex = Southeast_shdi)
+         meanpatcharea_decidousforest = Southeast_class4_MeanPatchArea,
+         meanpatcharea_evergreenforest = Southeast_class5_MeanPatchArea,
+         meanpatcharea_mixedforest = Southeast_class6_MeanPatchArea,
+         meanpatcharea_grassland = Southeast_class8_MeanPatchArea,
+         meanpatcharea_cropland = Southeast_class9_MeanPatchArea
+         #ShannonDiversityIndex = Southeast_shdi # Leaving this here while I fix layer
+    )
 
 #        [Changes to Data]                                                  ####
 
@@ -82,6 +88,9 @@ data_southeast$sex <- ifelse(data_southeast$sex == F, "F", "M")
 data_north$choice <- ifelse(data_north$choice$choice == "used",1,0)
 data_south$choice <- ifelse(data_south$choice$choice == "used",1,0)
 data_southeast$choice <- ifelse(data_southeast$choice == "used",1,0)
+
+# Changing NAs to zero
+data_southeast[is.na(data_southeast)] = 0
 
 ###############################################################################
 #   [Covariate Exploration]                                                 ####
@@ -144,13 +153,16 @@ ggsave(filename = "south_boxwhisk.png",
 #      [Southeast]                                                          ####
 #        [Correlation Plots]                                                ####
 
-southeast_cor_bargraph <-corr_cross(data_southeast[13:ncol(data_southeast)], rm.na = T, max_pvalue = 0.05, 
+southeast_cor_bargraph <-corr_cross(data_southeast[13:ncol(data_southeast)], rm.na = T, max_pvalue = 0.05,
                                 top = 10, grid = T)
 southeast_cor_bargraph
 
 ggsave(filename = "southeast_corplot.png",
        plot = southeast_cor_bargraph,
        device = "png",
+       width = 12,
+       height = 8,
+       units = "in",
        path = "2.Chapter1/3.Output/CovariateExploration")
 
 #        [Box-Whisker Plots]                                                ####
@@ -169,10 +181,6 @@ ggsave(filename = "southeast_boxwhisk.png",
        height = 6,
        units = "in")
 
-
-
-
-
 ###############################################################################
 #   [Models]                                                                ####
 #      [Southeast]                                                          ####
@@ -181,11 +189,10 @@ ggsave(filename = "southeast_boxwhisk.png",
 #               [Model 1]                                                   ####
 
 # Global Model with all covariates included 
-southeast_model_global <- data_southeast %>% 
+southeast_model_01 <- data_southeast %>% 
   fit_clogit(choice ~ contagion + 
                landscapeshapeindex +
                meanshapeindex + 
-               ShannonDiversityIndex +
                proportion_water + 
                proportion_wetland + 
                proportion_developed + 
@@ -196,11 +203,16 @@ southeast_model_global <- data_southeast %>%
                proportion_shrub + 
                proportion_grassland +
                proportion_cropland + 
+               meanpatcharea_decidousforest +
+               meanpatcharea_evergreenforest +
+               meanpatcharea_mixedforest +
+               meanpatcharea_grassland +
+               meanpatcharea_cropland +
                strata(observation_id))
 
-summary(southeast_model_global)
+summary(southeast_model_01)
 
-saveRDS(southeast_model_global, file = "2.Chapter1/3.Output/Models/Southeast_model1_global.rda")
+saveRDS(southeast_model_01, file = "2.Chapter1/3.Output/Models/Southeast_model_01.rda")
 
 #               [Model 2]                                                   ####
 
@@ -209,7 +221,6 @@ southeast_model_02 <- data_southeast %>%
   fit_clogit(choice ~ contagion + 
                landscapeshapeindex +
                meanshapeindex + 
-               ShannonDiversityIndex +
                proportion_water + 
                proportion_wetland + 
                proportion_developed + 
@@ -219,6 +230,11 @@ southeast_model_02 <- data_southeast %>%
                proportion_mixedforest + 
                proportion_shrub + 
                proportion_grassland +
+               meanpatcharea_decidousforest +
+               meanpatcharea_evergreenforest +
+               meanpatcharea_mixedforest +
+               meanpatcharea_grassland +
+               meanpatcharea_cropland +
                strata(observation_id))
 
 summary(southeast_model_02)
@@ -229,11 +245,12 @@ saveRDS(southeast_model_02, file = "2.Chapter1/3.Output/Models/Southeast_model_0
 
 #               [Model 3]                                                   ####
 
-# Removed meanshapeindex. It had a p-value of 0.50971 
+# Removed mean patch grassland. Highly correlated with proportion grassland. 
+
 southeast_model_03 <- data_southeast %>% 
   fit_clogit(choice ~ contagion + 
                landscapeshapeindex +
-               ShannonDiversityIndex +
+               meanshapeindex + 
                proportion_water + 
                proportion_wetland + 
                proportion_developed + 
@@ -242,7 +259,11 @@ southeast_model_03 <- data_southeast %>%
                proportion_evergreenforest + 
                proportion_mixedforest + 
                proportion_shrub + 
-               proportion_grassland +
+               meanpatcharea_decidousforest +
+               meanpatcharea_evergreenforest +
+               meanpatcharea_mixedforest +
+               meanpatcharea_grassland +
+               meanpatcharea_cropland +
                strata(observation_id))
 
 summary(southeast_model_03)
@@ -251,10 +272,10 @@ saveRDS(southeast_model_03, file = "2.Chapter1/3.Output/Models/Southeast_model_0
 
 #               [Model 4]                                                   ####
 
-# Removed contagion It had a p-value of 0.05220 
+# Removed meanshapeindex It had a p-value of 0.81439
 southeast_model_04 <- data_southeast %>% 
-  fit_clogit(choice ~ landscapeshapeindex +
-               ShannonDiversityIndex +
+  fit_clogit(choice ~ contagion + 
+               landscapeshapeindex +
                proportion_water + 
                proportion_wetland + 
                proportion_developed + 
@@ -263,22 +284,25 @@ southeast_model_04 <- data_southeast %>%
                proportion_evergreenforest + 
                proportion_mixedforest + 
                proportion_shrub + 
-               proportion_grassland +
+               meanpatcharea_decidousforest +
+               meanpatcharea_evergreenforest +
+               meanpatcharea_mixedforest +
+               meanpatcharea_grassland +
+               meanpatcharea_cropland +
                strata(observation_id))
+
 
 summary(southeast_model_04)
 
 saveRDS(southeast_model_04, file = "2.Chapter1/3.Output/Models/Southeast_model_04.rda")
 
-
-
-
 #               [Model 5]                                                   ####
 
-# Removed proportion of water. It had a p-value of 0.1691
+# Removed meanpatcharea decidousforest. It had a p-value of 0.81821
 southeast_model_05 <- data_southeast %>% 
-  fit_clogit(choice ~ landscapeshapeindex +
-               ShannonDiversityIndex +
+  fit_clogit(choice ~ contagion + 
+               landscapeshapeindex +
+               proportion_water + 
                proportion_wetland + 
                proportion_developed + 
                proportion_barren + 
@@ -286,35 +310,85 @@ southeast_model_05 <- data_southeast %>%
                proportion_evergreenforest + 
                proportion_mixedforest + 
                proportion_shrub + 
-               proportion_grassland +
+               meanpatcharea_evergreenforest +
+               meanpatcharea_mixedforest +
+               meanpatcharea_grassland +
+               meanpatcharea_cropland +
                strata(observation_id))
 
 summary(southeast_model_05)
 
 saveRDS(southeast_model_05, file = "2.Chapter1/3.Output/Models/Southeast_model_05.rda")
+
+#               [Model 6]                                                   ####
+
+# Removed meanpatcharea cropland It had a p-value of 0.642012
+southeast_model_06 <- data_southeast %>% 
+  fit_clogit(choice ~ contagion + 
+               landscapeshapeindex +
+               proportion_water + 
+               proportion_wetland + 
+               proportion_developed + 
+               proportion_barren + 
+               proportion_decidousforest +
+               proportion_evergreenforest + 
+               proportion_mixedforest + 
+               proportion_shrub + 
+               meanpatcharea_evergreenforest +
+               meanpatcharea_mixedforest +
+               meanpatcharea_grassland +
+               strata(observation_id))
+
+summary(southeast_model_06)
+
+saveRDS(southeast_model_06, file = "2.Chapter1/3.Output/Models/Southeast_model_06.rda")
+
+#               [Model 7]                                                   ####
+
+# Removed proportion barren. It had a p-value of 0.1230
+
+southeast_model_07 <- data_southeast %>% 
+  fit_clogit(choice ~ contagion + 
+               landscapeshapeindex +
+               proportion_water + 
+               proportion_wetland + 
+               proportion_developed + 
+               proportion_decidousforest +
+               proportion_evergreenforest + 
+               proportion_mixedforest + 
+               proportion_shrub + 
+               meanpatcharea_evergreenforest +
+               meanpatcharea_mixedforest +
+               meanpatcharea_grassland +
+               strata(observation_id))
+
+summary(southeast_model_07)
+
+saveRDS(southeast_model_07, file = "2.Chapter1/3.Output/Models/Southeast_model_07.rda")
+
 #               [Model Final]                                               ####
 
-southeast_model_05 <- data_southeast %>% 
-  fit_clogit(choice ~ landscapeshapeindex +
-               ShannonDiversityIndex +
+
+southeast_model_final <- data_southeast %>% 
+  fit_clogit(choice ~ contagion + 
+               landscapeshapeindex +
+               proportion_water + 
                proportion_wetland + 
                proportion_developed + 
-               proportion_barren + 
                proportion_decidousforest +
                proportion_evergreenforest + 
                proportion_mixedforest + 
                proportion_shrub + 
-               proportion_grassland +
+               meanpatcharea_evergreenforest +
+               meanpatcharea_mixedforest +
+               meanpatcharea_grassland +
                strata(observation_id))
 
-summary(southeast_model_05)
+summary(southeast_model_final)
 
-saveRDS(southeast_model_05, file = "2.Chapter1/3.Output/Models/Southeast_model_05.rda")
-
+saveRDS(southeast_model_final, file = "2.Chapter1/3.Output/Models/Southeast_model_final.rda")
 ###############################################################################
 #   [Model Validation and Inspection]                                       ####
-#      [Effect Plots]                                                       ####
-plot_model(southeast_model_05$model,
-           transform = "exp",
-           sort.est = T,
-           show.values = TRUE, value.offset = .3)
+#      [Southeast]                                                          ####
+
+#        [Effect Plots]                                                     ####
