192bfdd2820994b619315a0ad2470d49553359c6
Ortiz <wo165891@umconnect.umt.edu>
2022-09-13 13:56:59 -0600

    Initiation of availability sampling code.

---
+++ 2.Chapter1/1.Scripts/CovariateScripts/SamplingAvailability.R
@@ -0,0 +1,21 @@
+#   Script Details                                                          ####
+
+# Author: Waldemar Ortiz-Calo
+
+# Date:2022-09-13 
+
+# Purpose: This script is to sample availability within home range polygons and 
+# exporting those points.
+
+###############################################################################
+#   Library / Functions / Data                                              ####
+
+#      Library                                                              ####
+library(tidyverse)
+library(sf)
+
+#      Functions                                                            ####
+
+#      Data                                                                 ####
+
+###############################################################################
