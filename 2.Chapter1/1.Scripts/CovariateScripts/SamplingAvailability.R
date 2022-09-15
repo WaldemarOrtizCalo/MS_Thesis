d519c16c2c1360a98516636e48f9b56d567420a3
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2022-09-15 01:42:17 -0600

    Completed script to sample availability for the northern population.

--- 2.Chapter1/1.Scripts/CovariateScripts/SamplingAvailability.R
+++ 2.Chapter1/1.Scripts/CovariateScripts/SamplingAvailability.R
@@ -25,73 +25,71 @@ library(mapview)
 deer_df <- read_csv("1.DataManagement/RawData/csv_files/deer_all_revised.csv") %>% 
   mutate(choice = 1, .after = individual.local.identifier)
 
-#        [North]                                                            ####
+###############################################################################
+#   [Sampling Availability: North]                                          ####
+#      [Data Import and Prep]                                               ####
 
 # Locations
-north_locs <- deer_df %>% filter(site == "North")
+locs <- deer_df %>% filter(site == "North")
 
 # Polygon import
-north_hr_polygons <-  filepath_list %>% 
+
+filepath_list <- list.files("1.DataManagement/HomeRangePolygons/north/AggregatedPolygons",
+                            full.names = T,
+                            pattern = ".shp") %>% str_subset(pattern = "_[:upper:]_")
+
+hr_polygons <-  filepath_list %>% 
   lapply(st_read) %>% 
   do.call(bind_rows, .) %>% 
   mutate(season = rep(c("fall","spring","summer","winter"),4), .after = id) %>% 
   mutate(sex = (str_extract(filepath_list,pattern = "_[:upper:]_") %>% str_extract("[:upper:]")), .after = season) %>% 
   mutate(hr_type = ifelse(id == "homerange", "kde", "mcp"))
 
-#        [South]                                                            ####
+#      [Sampling Protocol]                                                  ####
 
+season_list <- unique(locs$season)
+sex_list <- unique(locs$sex)
 
-#        [Southeast]                                                        ####
-
-###############################################################################
-#   [Sampling Availability: North]                                          ####
-
-season_class <- "winter"
-sex_class <- "M"
-
-# Dataframe for season
-season_sf <- north_locs %>% 
-  filter(season == season_class & sex == sex_class) %>% 
-  st_as_sf(coords = c("location.long","location.lat"),
-           crs = 5070)
+for (i in 1:length(season_list)) {
   
-# Extracting relevant polygons
-mcp_hr_polygon <- north_hr_polygons %>% filter(hr_type == "mcp" & season == season_class & sex == sex_class)
-kde_hr_polygon <- north_hr_polygons %>% filter(hr_type == "kde" & season == season_class & sex == sex_class)
-
-# Determining used points
-used <- st_intersection(season_sf,mcp_hr_polygon)
-
-# Sampling available at a 1:5 ratio
-available <- st_sample(x = mcp_hr_polygon,
-                      size = (nrow(used)*5))
-
-available <- mcp_hr_polygon %>% 
-  st_sample(size = 200) %>% 
-  st_as_sf() %>% 
-  mutate(choice = 0,.before = 1) %>% 
-  rename(geometry = x)
-
-# Joining sf objects
-
-final <- bind_rows(used, available)
-
-st_write(final,
-         dsn = paste0("1.DataManagement\\used_available\\north\\","north_",sex_class,"_",season_class,".shp"),
-         layer = paste0("north_",sex_class,"_",season_class),
-         driver = "ESRI Shapefile")
-
-
-st_write(final, "crime.csv", layer_options = "GEOMETRY=AS_XY")
-
-st_write(final, dsn = "nc1.shp", layer = "nc.shp", driver = "ESRI Shapefile")
-
-test <- st_read(paste0("1.DataManagement\\used_available\\north\\","north_",sex_class,"_",season_class,".shp"))
-
-mapview(final[1:100,])
-
-
-
+  # Update Line
+  print(paste(i, "out of",length(season_list), "seasons"))
+  
+  for(j in 1:length(sex_list)){
+    
+    # Update Line
+    print(paste(j, "out of",length(sex_list), "sexes"))
+    
+    # Dataframe for season
+    season_sf <- locs %>% 
+      filter(season == season_list[i] & sex == sex_list[j]) %>% 
+      st_as_sf(coords = c("location.long","location.lat"),
+               crs = 5070)
+    
+    # Extracting relevant polygons
+    kde_hr_polygon <- hr_polygons %>% filter(hr_type == "kde" & season == season_list[i] & sex == sex_list[j])
+    
+    # Determining used points
+    used <- st_intersection(season_sf,kde_hr_polygon) %>% 
+      select(choice, geometry)
+    
+    # Sampling available at a 1:5 ratio
+    available <- st_sample(x = kde_hr_polygon, size = (nrow(used)*5)) %>% 
+      st_as_sf() %>% 
+      mutate(choice = 0,.before = 1) %>% 
+      rename(geometry = x) 
+    
+    # Joining sf objects
+    
+    final <- bind_rows(used, available)
+    
+    st_write(final,
+             dsn = paste0("1.DataManagement/used_available/north/","north_",sex_list[j],"_",season_list[i],".shp"),
+             layer = paste0("north_",sex_list[j],"_",season_list[i]),
+             driver = "ESRI Shapefile",
+             append = F)
+  }
+}
 
 ###############################################################################
 #   [Sampling Availability: South]                                          ####
