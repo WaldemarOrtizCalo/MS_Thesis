cfe7cdb5cc9e6c913529deece48380318fe9333f
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2022-09-15 14:20:00 -0600

    Creating of the new covariate extraction script.

---
+++ 2.Chapter1/1.Scripts/CovariateScripts/CovExtraction_Revised.R
@@ -0,0 +1,44 @@
+#   Script Details                                                          ####
+
+# Author: Waldemar Ortiz-Calo
+
+# Date:2022-09-15 
+
+# Purpose: 
+
+###############################################################################
+#   Library / Functions / Data                                              ####
+
+#      Library                                                              ####
+library(tidyverse)
+library(sf)
+library(terra)
+
+#      Functions                                                            ####
+
+#      Data                                                                 ####
+#        [Location Data]                                                    ####
+#        [Covariate Layers]                                                 ####
+
+# North
+covs_north <- list.files(path = "1.DataManagement/CovRasters/cov_layers_final/north",
+                         full.names = T,
+                         pattern = ".tif") %>% 
+  str_subset(pattern = "aux",negate = T) %>%
+  rast()
+
+# South
+covs_south <- list.files(path = "1.DataManagement/CovRasters/cov_layers_final/south",
+                         full.names = T,
+                         pattern = ".tif") %>% 
+  str_subset(pattern = "aux",negate = T) %>%
+  rast()
+
+# Southeast
+covs_southeast <- list.files(path = "1.DataManagement/CovRasters/cov_layers_final/southeast",
+                             full.names = T,
+                             pattern = ".tif") %>% 
+  str_subset(pattern = "aux",negate = T) %>%
+  rast()
+
+###############################################################################
