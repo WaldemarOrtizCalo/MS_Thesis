baee8cc077efa8e647b21e7a6612bf02f35e53b5
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2022-11-21 13:17:32 -0700

    Initialization of covariate extraction script.

---
+++ 2.Chapter1/1.Scripts/CovariateScripts/CovExtraction_Landscape.R
@@ -0,0 +1,22 @@
+#   Script Details                                                          ####
+
+# Author: Waldemar Ortiz-Calo
+
+# Date:2022-11-21 
+
+# Purpose: 
+
+###############################################################################
+#   Library / Functions / Data                                              ####
+
+#      Library                                                              ####
+
+#      Functions                                                            ####
+
+#      Data                                                                 ####
+
+#        Deer Locations                                                     ####
+
+#        Covariate Layers                                                   ####
+
+###############################################################################
