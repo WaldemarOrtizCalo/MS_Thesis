57e56bcb1a351f0aa7deff0b319e359ced0240a9
Ortiz <wo165891@umconnect.umt.edu>
2022-11-28 01:07:39 -0700

    Progress on Polygon creation and sampling availability.

--- 2.Chapter1/1.Scripts/CovariateScripts/CovExtraction_Landscape.R
+++ 2.Chapter1/1.Scripts/CovariateScripts/CovExtraction_Landscape.R
@@ -10,6 +10,10 @@
 #   Library / Functions / Data                                              ####
 
 #      Library                                                              ####
+library(tidyverse)
+library(sf)
+library(terra)
+library(adehabitatHR)
 
 #      Functions                                                            ####
 
@@ -17,6 +21,46 @@
 
 #        Deer Locations                                                     ####
 
-#        Covariate Layers                                                   ####
+deer_all <- read_csv("1.DataManagement/CleanData/deer_all_revised.csv") 
+
+deer_all %>% group_by(sex,age, .add = T) %>% 
+  summarise(n = n())
+
+###############################################################################
+#   North                                                                   ####
+#      Data Import                                                          ####
+#        Deer locations                                                     ####
+
+deer <- deer_all %>% filter(site == "North")
+
+deer_male <- deer %>% filter(sex == "M")
+
+deer_female <- deer %>% filter(sex == "F")
+
+#        Covariate Rasters                                                  ####
+
+cov_rasts <- list.files("1.DataManagement/CovRasters_Landscape/north",
+                        full.names = T) %>% 
+  str_subset("patches",negate = T) %>% 
+  str_subset("patcharea",negate = T) %>% rast()
+
+#      Creating Sampling Polygons                                           ####
+
+
+
+
+
+
+spdf <- data_aggregated
+coordinates(spdf) <- ~location.long + location.lat
+proj4string(spdf) <- CRS("+init=epsg:5070")
+
+kde <- kernelUD(spdf, h = "href")
+kde_UD <- getverticeshr(kde, 95)%>% 
+  st_as_sf() %>% 
+  st_write("1.DataManagement/HomeRangePolygons/north/AggregatedPolygons/kde_north.shp",
+           append=FALSE)
+
+
 
 ###############################################################################
