78619a6ac7d6b60b7a280789e8cb9fc7037f07b1
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2022-12-04 15:52:01 -0700

    finished prepping modeling code and setting up computing cluster

--- 2.Chapter1/1.Scripts/ModelingScripts/final_LogisticRegression.R
+++ 2.Chapter1/1.Scripts/ModelingScripts/final_LogisticRegression.R
@@ -12,6 +12,8 @@
 #      Library                                                              ####
 library(tidyverse)
 library(lares)
+library(foreach)
+library(doParallel)
 
 #      Functions                                                            ####
 
@@ -20,20 +22,35 @@ library(lares)
 data_north <- list.files("1.DataManagement/ch1_data/north/model_data",
                          full.names = T,pattern = ".csv")
 
+names_north <- list.files("1.DataManagement/ch1_data/north/model_data",
+                         full.names = F,pattern = ".csv") %>% 
+  str_remove(".csv") %>% 
+  str_replace("final","model")
+
 #        South                                                              ####
 data_south <- list.files("1.DataManagement/ch1_data/south/model_data",
                          full.names = T,pattern = ".csv")
+
+names_south <- list.files("1.DataManagement/ch1_data/south/model_data",
+                          full.names = F,pattern = ".csv") %>% 
+  str_remove(".csv") %>% 
+  str_replace("final","model")
 
 #        Southeast                                                          ####
 data_southeast <- list.files("1.DataManagement/ch1_data/southeast/model_data",
                          full.names = T,pattern = ".csv")
 
+names_southeast <- list.files("1.DataManagement/ch1_data/southeast/model_data",
+                          full.names = F,pattern = ".csv") %>% 
+  str_remove(".csv") %>% 
+  str_replace("final","model")
+
 ###############################################################################
 #   North                                                                   ####
 #      Data                                                                 ####
 #        Data Import                                                        ####
-
 data <- lapply(data_north, read_csv)
+
 #        Correlation Plots                                                  ####
 
 for (i in 1:length(data_north)) {
@@ -61,6 +78,60 @@ for (i in 1:length(data_north)) {
 }
 
 
+
+#      Models                                                               ####
+#        Dredge Models                                                      ####
+#           Start of Cluster                                                ####
+
+# Cluster Number
+cl <- makeCluster(4)
+registerDoParallel(cl)
+
+# Exporting Packages
+clusterEvalQ(cl,
+             {
+               library(tidyverse)
+               library(MuMIn)
+               library(foreach)
+               library(tidyverse)
+             })
+
+# Exporting data to clusters
+clusterExport(cl=cl, varlist=c("data","names_north"), envir=environment())
+
+#           Dredge                                                          ####
+foreach(i = 1:length(data)) %dopar% {
+
+  # Separating specific dataframe
+  df <- data[[i]]
+  name <- names_north[[i]]
+  
+  # Making the list of covariate names and eliminating undesired covariates
+  covs <- names(df) %>% 
+    .[(str_which(.,"geometry")+1):length(.)] %>% 
+    str_subset("shrub",negate = T) %>% 
+    str_subset("wetland",negate = T) %>% 
+    str_subset("barren",negate = T) 
+  
+  # Building the formula
+  formula <- as.formula(paste("choice ~ ", paste(covs, collapse= "+")))
+  
+  # Model
+  model <- glm(formula = formula, 
+               data = df, 
+               family = "binomial",na.action = "na.fail")
+  
+  # Model Dredging
+  dredge <- dredge(model)
+  
+  # Dredge Export
+  write_csv(x = dredge,
+            file = paste0("2.Chapter1/3.Output/models_dredge_landscapelevel/",names_north[i],"_dredge.csv"),
+            append = F)
+  
+  # Return of the for loop
+  print(i)
+}
 
 ###############################################################################
 #   South                                                                   ####
