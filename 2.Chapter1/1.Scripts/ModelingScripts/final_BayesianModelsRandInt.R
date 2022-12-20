54bae33d00e90af4cab4f01d3cfb159cd0ffbd1f
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2022-12-20 14:30:03 -0700

    prepared script for random intercept model

--- 2.Chapter1/1.Scripts/ModelingScripts/final_BayesianModelsRandInt.R
+++ 2.Chapter1/1.Scripts/ModelingScripts/final_BayesianModelsRandInt.R
@@ -16,8 +16,9 @@ library(doParallel)
 library(MuMIn)
 library(jtools)
 library(rstanarm)
-#      Functions                                                            ####
+library(brms)
 
+#      Functions                                                            ####
 #      Data                                                                 ####
 #        North                                                              ####
 data_north <- list.files("1.DataManagement/ch1_data/north/model_data",
@@ -99,8 +100,90 @@ for (i in 1:length(data_north)) {
   print(i)
 }
 
+#        UsedAvail Plots                                                    ####
+
+for (i in 1:length(data_north)) {
+  
+  # Iteration Monitor 1 
+  print(paste0("Starting Dataset ",i, " out of ",length(data_north)))
+  
+  # Dataframe Extraction and Name
+  df <- data[[i]]
+  name <- list.files("1.DataManagement/ch1_data/north/model_data",
+                     full.names = F,pattern = ".csv") %>% 
+    .[[i]] %>% 
+    str_remove(".csv") %>% 
+    str_replace("final","_usedavail")
+  
+  # Making a List of Covariates
+  covs <- names(df) %>% 
+    .[(str_which(.,"geometry")+1):length(.)] %>% 
+    str_subset("shrub",negate = T) %>% 
+    str_subset("wetland",negate = T) %>% 
+    str_subset("barren",negate = T) %>% 
+    str_subset("water",negate = T)  %>%
+    str_subset("TRI",negate = T) %>% 
+    str_subset("developed",negate = T) %>% 
+    append("choice",after = 0) %>% 
+    c()
+  
+  # Covariate List 
+  covlist <- foreach(v = 1:length(covs),.combine = c) %do% 
+    {str_which(names(df),pattern = covs[v])} %>% 
+    unique()
+  
+  # Subsetting Covariates from dataframe
+  cov_df <- df[covlist]
+  
+  # Preparing the data in box and whisker plot format
+  boxwhisk_data <- pivot_longer(cov_df,
+                                cols = 2:ncol(cov_df))
+  
+  # Replacing NA values with Zero
+  boxwhisk_data$value <- ifelse(is.na(boxwhisk_data$value),
+                                0,
+                                boxwhisk_data$value)
+  
+  # Extracting covariates Names
+  cov_name <- covs[2:length(covs)]
+  
+  # Export folder
+  export_dir <- paste0("2.Chapter1/3.Output/covariate_analysis/north/used_avail_plots","/",name)
+  
+  for (v in 1:length(cov_name)) {
+    
+    # Box and Whisker Plot
+    boxwhisk_plot <- ggplot(subset(boxwhisk_data, name == cov_name[[v]]),
+                            aes(x = name, y = value, fill = as.factor(choice)))+
+      geom_boxplot()+
+      labs(fill = "Choice", x = "Covariate") +
+      theme_bw()
+    
+    # Plot Export
+    ggsave(filename = paste0(cov_name[[v]],".png"),
+           plot = boxwhisk_plot,
+           device = "png",
+           path = export_dir,
+           width = 6,
+           height = 6,
+           units = "in")
+    
+    # Iteration Monitor
+    print(paste0(v," out of ", length(cov_name)))
+  }
+}
+
 #      Model                                                                ####
-#        Bayesian Model                                                     ####
+#        [Model Settings]                                                   ####
+# Model Settings
+chains <- 3 
+iter <- 4000
+warmup <- iter*.25
+thin <- 1
+cores <- chains
+refresh <- iter * 0.05
+
+#        [Model]                                                            ####
 
 foreach(i = 1:length(data_north)) %do% {
   
@@ -119,28 +202,24 @@ foreach(i = 1:length(data_north)) %do% {
     .[(str_which(.,"geometry")+1):length(.)] %>% 
     str_subset("shrub",negate = T) %>% 
     str_subset("wetland",negate = T) %>% 
-    str_subset("barren",negate = T) 
+    str_subset("barren",negate = T) %>% 
+    str_subset("water",negate = T)  %>%
+    str_subset("TRI",negate = T) %>% 
+    str_subset("developed",negate = T) %>% 
+    c()
   
   # Building the formula
-  formula <- as.formula(paste("choice ~ ", paste(covs, collapse= "+")))
+  formula <- as.formula(paste("choice ~ ", 
+                              paste(covs, collapse= "+"),
+                              "+ (1 | individual.local.identifier)"))
   
   # Model
   
-  # Model Settings
-  chains <- 4 
-  iter <- 5000
-  warmup <- iter*.25
-  thin <- 1
-  cores <- chains
-  refresh <- iter/10
-  
-  model <- stan_glm(
+  model <- brm(
     formula = formula,
     data = df,
     family = binomial(link = "logit"),
-    prior_intercept = normal(0, 1),
-    prior = normal(0, 1),
-    QR = TRUE,
+    prior = prior(normal(0, 1)),
     refresh = refresh,
     chains = chains, 
     iter = iter,
@@ -150,10 +229,9 @@ foreach(i = 1:length(data_north)) %do% {
   )
   
   saveRDS(object = model,
-          file = paste0("2.Chapter1/3.Output/models_bayesian/north/bayesian_",name,".RDS"))
+          file = paste0("2.Chapter1/3.Output/models_bayesian_randomintercept/north/bayesian_",name,".RDS"))
   
-  return(print(paste0(i," out of ", length(data_north), " is completed")))
-  
+  return(print(paste0("North: ",i," out of ", length(data_north), " is completed")))
 }
 
 
@@ -183,6 +261,7 @@ for (i in 1:length(data_south)) {
     str_subset("TRI",negate = T) %>% 
     str_subset("developed",negate = T) %>%
     str_subset("evergreen",negate = T) %>%
+    str_subset("mixed",negate = T) %>%
     c()
   
   covlist <- foreach(v = 1:length(covs),.combine = c) %do% {str_which(names(df),pattern = covs[v])} %>% unique()
@@ -211,7 +290,144 @@ for (i in 1:length(data_south)) {
   print(i)
 }
 
-#      Models                                                               ####
+#        UsedAvail Plots                                                    ####
+
+for (i in 1:length(data_south)) {
+  
+  # Iteration Monitor 1 
+  print(paste0("Starting Dataset ",i, " out of ",length(data_south)))
+  
+  # Dataframe Extraction and Name
+  df <- data[[i]]
+  name <- list.files("1.DataManagement/ch1_data/south/model_data",
+                     full.names = F,pattern = ".csv") %>% 
+    .[[i]] %>% 
+    str_remove(".csv") %>% 
+    str_replace("final","_usedavail")
+  
+  # Making a List of Covariates
+  covs <- names(df) %>% 
+    .[(str_which(.,"geometry")+1):length(.)] %>% 
+    str_subset("shrub",negate = T) %>% 
+    str_subset("wetland",negate = T) %>% 
+    str_subset("barren",negate = T) %>% 
+    str_subset("water",negate = T)  %>%
+    str_subset("TRI",negate = T) %>% 
+    str_subset("developed",negate = T) %>%
+    str_subset("evergreen",negate = T) %>%
+    str_subset("mixed",negate = T) %>%
+    append("choice",after = 0) %>% 
+    c()
+  
+  # Covariate List 
+  covlist <- foreach(v = 1:length(covs),.combine = c) %do% 
+    {str_which(names(df),pattern = covs[v])} %>% 
+    unique()
+  
+  # Subsetting Covariates from dataframe
+  cov_df <- df[covlist]
+  
+  # Preparing the data in box and whisker plot format
+  boxwhisk_data <- pivot_longer(cov_df,
+                                cols = 2:ncol(cov_df))
+  
+  # Replacing NA values with Zero
+  boxwhisk_data$value <- ifelse(is.na(boxwhisk_data$value),
+                                0,
+                                boxwhisk_data$value)
+  
+  # Extracting covariates Names
+  cov_name <- covs[2:length(covs)]
+  
+  # Export folder
+  export_dir <- paste0("2.Chapter1/3.Output/covariate_analysis/south/used_avail_plots","/",name)
+  
+  for (v in 1:length(cov_name)) {
+    
+    # Box and Whisker Plot
+    boxwhisk_plot <- ggplot(subset(boxwhisk_data, name == cov_name[[v]]),
+                            aes(x = name, y = value, fill = as.factor(choice)))+
+      geom_boxplot()+
+      labs(fill = "Choice", x = "Covariate") +
+      theme_bw()
+    
+    # Plot Export
+    ggsave(filename = paste0(cov_name[[v]],".png"),
+           plot = boxwhisk_plot,
+           device = "png",
+           path = export_dir,
+           width = 6,
+           height = 6,
+           units = "in")
+    
+    # Iteration Monitor
+    print(paste0(v," out of ", length(cov_name)))
+  }
+}
+
+#      Model                                                                ####
+#        [Model Settings]                                                   ####
+# Model Settings
+chains <- 3 
+iter <- 4000
+warmup <- iter*.25
+thin <- 1
+cores <- chains
+refresh <- iter * 0.05
+
+#        [Model]                                                            ####
+
+foreach(i = 1:length(data_south)) %do% {
+  
+  # Separating specific dataframe
+  df_raw <- data[[i]]
+  name <- names_south[[i]]
+  
+  df <- df_raw
+  
+  for (j in (str_which(names(df),"geometry")+1):ncol(df)) {
+    df[,j] <- scale(df[,j])
+  }
+  
+  # Making the list of covariate names and eliminating undesired covariates
+  covs <- names(df) %>% 
+    .[(str_which(.,"geometry")+1):length(.)] %>% 
+    str_subset("shrub",negate = T) %>% 
+    str_subset("wetland",negate = T) %>% 
+    str_subset("barren",negate = T) %>% 
+    str_subset("water",negate = T)  %>%
+    str_subset("TRI",negate = T) %>% 
+    str_subset("developed",negate = T) %>%
+    str_subset("evergreen",negate = T) %>%
+    str_subset("mixed",negate = T) %>%
+    c()
+  
+  # Building the formula
+  formula <- as.formula(paste("choice ~ ", 
+                              paste(covs, collapse= "+"),
+                              "+ (1 | individual.local.identifier)"))
+  
+  # Model
+  
+  model <- brm(
+    formula = formula,
+    data = df,
+    family = binomial(link = "logit"),
+    prior = prior(normal(0, 1)),
+    refresh = refresh,
+    chains = chains, 
+    iter = iter,
+    warmup = warmup,
+    thin = thin,
+    cores = cores
+  )
+  
+  saveRDS(object = model,
+          file = paste0("2.Chapter1/3.Output/models_bayesian_randomintercept/south/bayesian_",name,".RDS"))
+  
+  return(print(paste0("South: ",i," out of ", length(data_south), " is completed")))
+}
+
 ###############################################################################
 #   Southeast                                                               ####
 #      Data                                                                 ####
@@ -238,6 +454,11 @@ for (i in 1:length(data_southeast)) {
     str_subset("TRI",negate = T) %>% 
     str_subset("developed",negate = T) %>% 
     str_subset("evergreen",negate = T) %>%
+    str_subset("mixed",negate = T) %>%
+    str_subset("grassland",negate = T) %>%
+    str_subset("southeast_patchdist_deciduous_small",negate = T) %>%
+    str_subset("southeast_dem",negate = T) %>%
+    str_subset("southeast_patchdist_deciduous_forest",negate = T) %>%
     c()
   
   covlist <- foreach(v = 1:length(covs),.combine = c) %do% {str_which(names(df),pattern = covs[v])} %>% unique()
@@ -266,73 +487,150 @@ for (i in 1:length(data_southeast)) {
   print(i)
 }
 
-#      Models                                                               ####
-###############################################################################
-#   [brm]                                                                   ####
-library(brms)
-i <- 1
-# Separating specific dataframe
-df_raw <- data[[i]]
-name <- names_north[[i]]
+#        UsedAvail Plots                                                    ####
 
-df <- df_raw
-
-for (j in (str_which(names(df),"geometry")+1):ncol(df)) {
-  df[,j] <- scale(df[,j])
+for (i in 1:length(data_southeast)) {
+  
+  # Iteration Monitor 1 
+  print(paste0("Starting Dataset ",i, " out of ",length(data_southeast)))
+  
+  # Dataframe Extraction and Name
+  df <- data[[i]]
+  name <- list.files("1.DataManagement/ch1_data/southeast/model_data",
+                     full.names = F,pattern = ".csv") %>% 
+    .[[i]] %>% 
+    str_remove(".csv") %>% 
+    str_replace("final","_usedavail")
+  
+  # Making a List of Covariates
+  covs <- names(df) %>% 
+    .[(str_which(.,"geometry")+1):length(.)] %>% 
+    str_subset("shrub",negate = T) %>% 
+    str_subset("wetland",negate = T) %>% 
+    str_subset("barren",negate = T) %>% 
+    str_subset("water",negate = T)  %>%
+    str_subset("TRI",negate = T) %>% 
+    str_subset("developed",negate = T) %>% 
+    str_subset("evergreen",negate = T) %>%
+    str_subset("mixed",negate = T) %>%
+    str_subset("grassland",negate = T) %>%
+    str_subset("southeast_patchdist_deciduous_small",negate = T) %>%
+    str_subset("southeast_dem",negate = T) %>%
+    str_subset("southeast_patchdist_deciduous_forest",negate = T) %>%
+    append("choice",after = 0) %>% 
+    c()
+  
+  # Covariate List 
+  covlist <- foreach(v = 1:length(covs),.combine = c) %do% 
+    {str_which(names(df),pattern = covs[v])} %>% 
+    unique()
+  
+  # Subsetting Covariates from dataframe
+  cov_df <- df[covlist]
+  
+  # Preparing the data in box and whisker plot format
+  boxwhisk_data <- pivot_longer(cov_df,
+                                cols = 2:ncol(cov_df))
+  
+  # Replacing NA values with Zero
+  boxwhisk_data$value <- ifelse(is.na(boxwhisk_data$value),
+                                0,
+                                boxwhisk_data$value)
+  
+  # Extracting covariates Names
+  cov_name <- covs[2:length(covs)]
+  
+  # Export folder
+  export_dir <- paste0("2.Chapter1/3.Output/covariate_analysis/southeast/used_avail_plots","/",name)
+  
+  for (v in 1:length(cov_name)) {
+    
+    # Box and Whisker Plot
+    boxwhisk_plot <- ggplot(subset(boxwhisk_data, name == cov_name[[v]]),
+                            aes(x = name, y = value, fill = as.factor(choice)))+
+      geom_boxplot()+
+      labs(fill = "Choice", x = "Covariate") +
+      theme_bw()
+    
+    # Plot Export
+    ggsave(filename = paste0(cov_name[[v]],".png"),
+           plot = boxwhisk_plot,
+           device = "png",
+           path = export_dir,
+           width = 6,
+           height = 6,
+           units = "in")
+    
+    # Iteration Monitor
+    print(paste0(v," out of ", length(cov_name)))
+  }
 }
 
-# Making the list of covariate names and eliminating undesired covariates
-covs <- names(df) %>% 
-  .[(str_which(.,"geometry")+1):length(.)] %>% 
-  str_subset("shrub",negate = T) %>% 
-  str_subset("wetland",negate = T) %>% 
-  str_subset("barren",negate = T) 
-
-# Building the formula
-formula <- as.formula(paste("choice ~ ", paste(covs[1:3], collapse= "+")))
-
-# Model
-
+#      Model                                                                ####
+#        [Model Settings]                                                   ####
 # Model Settings
-chains <- 4 
-iter <- 1000
+chains <- 3 
+iter <- 4000
 warmup <- iter*.25
 thin <- 1
 cores <- chains
-refresh <- iter/10
+refresh <- iter * 0.05
 
-model <- brm(
-  formula = formula,
-  data = df,
-  family = binomial(link = "logit"),
-  prior = prior(normal(0, 1)),
-  refresh = refresh,
-  chains = chains, 
-  iter = iter,
-  warmup = warmup,
-  thin = thin,
-  cores = cores
-)
+#        [Model]                                                            ####
 
-saveRDS(object = model,
-        file = paste0("2.Chapter1/3.Output/models_bayesian/north/bayesian_",name,".RDS"))
-
-return(print(paste0(i," out of ", length(data_north), " is completed")))
-
-
+foreach(i = 1:length(data_southeast)) %do% {
+  
+  # Separating specific dataframe
+  df_raw <- data[[i]]
+  name <- names_southeast[[i]]
+  
+  df <- df_raw
+  
+  for (j in (str_which(names(df),"geometry")+1):ncol(df)) {
+    df[,j] <- scale(df[,j])
+  }
+  
+  # Making the list of covariate names and eliminating undesired covariates
+  covs <- names(df) %>% 
+    .[(str_which(.,"geometry")+1):length(.)] %>% 
+    str_subset("shrub",negate = T) %>% 
+    str_subset("wetland",negate = T) %>% 
+    str_subset("barren",negate = T) %>% 
+    str_subset("water",negate = T)  %>%
+    str_subset("TRI",negate = T) %>% 
+    str_subset("developed",negate = T) %>% 
+    str_subset("evergreen",negate = T) %>%
+    str_subset("mixed",negate = T) %>%
+    str_subset("grassland",negate = T) %>%
+    str_subset("southeast_patchdist_deciduous_small",negate = T) %>%
+    str_subset("southeast_dem",negate = T) %>%
+    str_subset("southeast_patchdist_deciduous_forest",negate = T) %>%
+    c()
+  
+  # Building the formula
+  formula <- as.formula(paste("choice ~ ", 
+                              paste(covs, collapse= "+"),
+                              "+ (1 | individual.local.identifier)"))
+  
+  # Model
+  
+  model <- brm(
+    formula = formula,
+    data = df,
+    family = binomial(link = "logit"),
+    prior = prior(normal(0, 1)),
+    refresh = refresh,
+    chains = chains, 
+    iter = iter,
+    warmup = warmup,
+    thin = thin,
+    cores = cores
+  )
+  
+  saveRDS(object = model,
+          file = paste0("2.Chapter1/3.Output/models_bayesian_randomintercept/southeast/bayesian_",name,".RDS"))
+  
+  return(print(paste0("Southeast: ",i," out of ", length(data_southeast), " is completed")))
+}
 
 ###############################################################################
-formula_randint <- as.formula(paste("choice ~ ", paste(covs[1:3], collapse= "+")," + (1 | ID)"))
-
-model_brm_randint <- brm(
-  formula = formula_randint,
-  data = df,
-  family = binomial(link = "logit"),
-  prior = prior(normal(0, 1)),
-  refresh = 1,
-  chains = chains, 
-  iter = iter,
-  warmup = warmup,
-  thin = thin,
-  cores = cores
-)
