480492ca7b08db2f6befa04e620388ce8ddff3be
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2023-01-27 17:37:16 -0700

    Started process of cleaning data

--- 3.Chapter2/1.Scripts/data_cleaning/cleaning_animal_locs.R
+++ 3.Chapter2/1.Scripts/data_cleaning/cleaning_animal_locs.R
@@ -8,11 +8,45 @@
 
 ###############################################################################
 #   Library / Functions / Data                                              ####
-
 #      Library                                                              ####
+library(tidyverse)
+library(readxl)
 
 #      Functions                                                            ####
 
 #      Data                                                                 ####
-table_mortality <- 
+
+# Importing Mortality table
+table_mortality_original <- read_xlsx("1.DataManagement\\ch2_data\\dbo_tblMortality.xlsx")
+
+# Deer Locations
+locs_deer <- read_csv("1.DataManagement\\CleanData\\deer_all_revised.csv")
+
 ###############################################################################
+#   Summary of Data                                                         ####
+summary_mortality <- table_mortality_original %>% 
+  group_by(StudyArea,Sex,EstAge) %>% 
+  summarise(n()) %>% 
+  na.omit() %>%
+  filter(StudyArea %in% c("N","S")) %>% 
+  filter(EstAge %in% c("F","Y","A")) %>% 
+  mutate(StudyArea = factor(StudyArea, levels = c("N","S"))) %>%
+  mutate(Sex = factor(Sex, levels = c("M","F"))) %>% 
+  mutate(EstAge = factor(EstAge, levels = c("F","Y","A"))) %>% 
+  arrange(StudyArea,Sex, desc(EstAge),.by_group = T)
+
+summary_mortality
+
+###############################################################################
+#   Data Prep                                                               ####
+
+# Cleaning Mortality table
+mortality_table <- table_mortality_original %>% 
+  na.omit() %>%
+  filter(StudyArea %in% c("N","S")) %>% 
+  filter(EstAge %in% c("F","Y","A")) %>% 
+  mutate(StudyArea = factor(StudyArea, levels = c("N","S"))) %>%
+  mutate(Sex = factor(Sex, levels = c("M","F"))) %>% 
+  mutate(EstAge = factor(EstAge, levels = c("F","Y","A"))) %>% 
+  arrange(StudyArea,Sex, desc(EstAge),.by_group = T)
+
