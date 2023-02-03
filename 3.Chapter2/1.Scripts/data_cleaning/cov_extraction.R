eeb0a36a26d91173a9bdb406776465474c824841
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2023-02-02 22:34:25 -0700

    started building home range estimation script

--- 3.Chapter2/1.Scripts/data_cleaning/cov_extraction.R
+++ 3.Chapter2/1.Scripts/data_cleaning/cov_extraction.R
@@ -32,3 +32,23 @@ covlayers_south <- list.files("1.DataManagement/CovRasters/base_layers",
                               full.names = T) %>% str_subset(".xml", negate = T) %>% str_subset("southeast", negate = T)
 
 ###############################################################################
+#   North                                                                   ####
+#      Data Import                                                          ####
+
+# Deer Locations
+locs <- locs_deer %>% filter(site == "North")
+
+# Covariate Layers/Stack 
+cov_layers <- rast(covlayers_north)
+
+#      Home Range Calculation                                               ####
+#        Filtering ids and periods with greater than 20 locs                ####
+
+valid_homeranges <- locs %>% 
+  group_by(individual.local.identifier,ordered_int_id) %>% 
+  summarize(n_locs = n()) %>% 
+  filter(n_locs > 20)
+
+###############################################################################
+#   South                                                                   ####
+###############################################################################
