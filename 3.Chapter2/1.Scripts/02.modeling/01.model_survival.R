b84ac6f366145d90160d84f732e1f095a2a674d8
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2023-02-06 13:20:40 -0700

    Started Model dev

---
+++ 3.Chapter2/1.Scripts/02.modeling/01.model_survival.R
@@ -0,0 +1,22 @@
+#   Script Details                                                          ####
+
+# Author: Waldemar Ortiz-Calo
+
+# Date:2023-02-06 
+
+# Purpose: 
+
+###############################################################################
+#   Library / Functions / Data                                              ####
+
+#      Library                                                              ####
+
+#      Functions                                                            ####
+
+#      Data                                                                 ####
+
+locs_deer <- read_csv("1.DataManagement/ch2_data/clean/locs/deer_mortalitylocs.csv")
+
+
+
+###############################################################################
