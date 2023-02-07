417f8505a695b817c1b9562b2046a08254091578
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2023-02-06 20:48:23 -0700

    Joined status and cov data

--- 3.Chapter2/1.Scripts/02.modeling/01.model_survival.R
+++ 3.Chapter2/1.Scripts/02.modeling/01.model_survival.R
@@ -8,15 +8,56 @@
 
 ###############################################################################
 #   Library / Functions / Data                                              ####
-
 #      Library                                                              ####
+library(tidyverse)
+library(survival)
 
 #      Functions                                                            ####
 
 #      Data                                                                 ####
+#        [Locs]                                                             ####
 
-locs_deer <- read_csv("1.DataManagement/ch2_data/clean/locs/deer_mortalitylocs.csv")
+# Deer Locs
+locs_deer <- read_csv("1.DataManagement/ch2_data/clean/locs/deer_mortalitylocs.csv") %>% 
+  mutate(age = factor(age,levels = c("A","Y","F")))
 
+# Deer metadata
+locs_metadata <- locs_deer %>% 
+  dplyr::select(c(individual.local.identifier,ordered_int_id,sex,site,age,t_start,t_end,event)) %>% 
+  unique()
+
+# Identifying problem cases
+problem_cases <- locs_metadata %>% 
+  group_by(individual.local.identifier,ordered_int_id) %>% 
+  summarize(count = n()) %>% 
+  filter(count > 1)
+
+# Cleaning
+for (i in 1:nrow(problem_cases)) {
+  
+  sub <- locs_metadata[locs_metadata$individual.local.identifier == problem_cases[[i,1]] & 
+                         locs_metadata$ordered_int_id == problem_cases[[i,2]],] 
+  
+  age_class <- sub$age %>% .[which.min(.)]
+  
+  locs_metadata[locs_metadata$individual.local.identifier == problem_cases[[i,1]] & 
+                  locs_metadata$ordered_int_id == problem_cases[[i,2]], "age"] <- age_class
+  
+  print(i)
+}
+
+# Identifying clean ones
+locs_metadata_clean <- locs_metadata %>% unique()
+
+#        [Covs]                                                             ####
+
+# This will be replaced with the join cov dataframe
+covs <- read_csv("1.DataManagement/ch2_data/clean/cov_extractions/north_final_covs.csv")
+
+#        [Joining Covs and Loc data]                                        ####
+
+data_final <- covs %>% 
+  left_join(locs_metadata_clean)
 
 
 ###############################################################################
