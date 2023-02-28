f72b8e6aeb0f0809caa1a7c6a438f34a94d313e7
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2023-02-28 11:30:12 -0700

    New cleaning script for locs data

--- 1.DataManagement/Scripts/NewCleaningScript.R
+++ 1.DataManagement/Scripts/NewCleaningScript.R
@@ -20,7 +20,35 @@ library(foreach)
 #      Data                                                                 ####
 
 # Deer Locations 
-locs_deer <- "1.DataManagement\\loc_data\\deer_all_NS_clean.csv" %>% 
+locs_deer_raw <- "1.DataManagement\\loc_data\\deer_all_NS_clean.csv" %>% 
   read_csv()
+
+###############################################################################
+#   1.Renaming Columns                                                      ####
+
+locs_deer_cleaning <- locs_deer_raw %>% 
+  rename(individual.local.identifier = id,
+         location.long = x,
+         location.lat = y,
+         timestamp = t)
+
+#   2. Removing Unnecessary Columns                                         ####
+
+locs_deer_cleaning <- locs_deer_cleaning %>% 
+  select(-c(year,id_yr))
+
+#   3. Adding new time covariates                                           ####
+
+locs_deer_cleaning <- locs_deer_cleaning %>% 
+  mutate(timestamp = mdy_hm(timestamp)) %>% 
+  mutate(year = year(timestamp),
+         month = month(timestamp))
+  
+#   4. Adding Study Site                                                    ####
+
+locs_deer_cleaning <- locs_deer_cleaning %>% 
+  mutate(site= ifelse(str_sub(locs_deer_cleaning$individual.local.identifier,1,1) == "N",
+                      "North","South"))
+
 
 ###############################################################################
