a158c7f0c599021657a2dd7571cd236715e3a525
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2023-03-02 10:24:59 -0700

    Added exemptions for the southeast study area

--- 1.DataManagement/Scripts/NewCleaningScript.R
+++ 1.DataManagement/Scripts/NewCleaningScript.R
@@ -20,7 +20,7 @@ library(foreach)
 #      Data                                                                 ####
 
 # Deer Locations 
-locs_deer_raw <- "1.DataManagement\\loc_data\\deer_all_NS_clean.csv" %>% 
+locs_deer_raw <- "1.DataManagement\\loc_data\\deer_nsse_final_clean.csv" %>% 
   read_csv()
 
 ###############################################################################
@@ -40,21 +40,21 @@ locs_deer_cleaning <- locs_deer_cleaning %>%
 #   3. Adding new time covariates                                           ####
 
 locs_deer_cleaning <- locs_deer_cleaning %>% 
-  mutate(timestamp = mdy_hm(timestamp)) %>% 
   mutate(year = year(timestamp),
          month = month(timestamp))
   
 #   4. Adding Study Site                                                    ####
 
 locs_deer_cleaning <- locs_deer_cleaning %>% 
-  mutate(site= ifelse(str_sub(locs_deer_cleaning$individual.local.identifier,1,1) == "N",
-                      "North","South"))
-
-
+  mutate(site = ifelse(str_sub(locs_deer_cleaning$individual.local.identifier,1,1) == "N","North",
+                      ifelse(str_sub(locs_deer_cleaning$individual.local.identifier,1,1) == "S",
+                      "South",NA))) %>% 
+  mutate(site = ifelse(str_sub(locs_deer_cleaning$individual.local.identifier,1,3) == "SCF","Southeast",site))
+  
 ###############################################################################
 #   Export                                                                  #### 
 
 write_csv(locs_deer_cleaning,
-          file = "1.DataManagement/loc_data/deer_NS_final_clean.csv")
+          file = "1.DataManagement/loc_data/deer_all_final_clean.csv")
 
 ###############################################################################
