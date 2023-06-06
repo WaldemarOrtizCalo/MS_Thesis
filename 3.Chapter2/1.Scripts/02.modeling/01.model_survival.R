93961f0286b435182f29bbce26e239faeb5e7262
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2023-06-06 00:07:44 -0600

    initializing models

--- 3.Chapter2/1.Scripts/02.modeling/01.model_survival.R
+++ 3.Chapter2/1.Scripts/02.modeling/01.model_survival.R
@@ -66,6 +66,19 @@ loc_timedata <- read_csv("1.DataManagement/ch2_data/clean/locs/deer_mortalityloc
          tduration = tend - tstart) %>% 
   select(- c(event_day))
 
+#        Adding Sex and Age                                                 ####
+
+# Dataframe with demographic data
+deer_age_sex <- read_csv("1.DataManagement/ch2_data/clean/locs/deer_mortalitylocs.csv") %>% 
+  mutate(age = factor(age,levels = c("F", "Y" , "A")),
+         sex = factor(sex,levels = c("F", "M" ))) %>% 
+  group_by(individual.local.identifier,ordered_int_id) %>% 
+  summarize(sex = names(which.max(table(sex))),
+            age = names(which.max(table(age)))) %>% 
+  rename("id" = "individual.local.identifier",
+         "int_id"= "ordered_int_id")
+
+
 #      Cluster Setup                                                        ####
 
 # Cluster Number
@@ -105,14 +118,21 @@ loc_data_final <- left_join(hr_cov_data,
                             by = c("id",
                                    "int_id")) %>% 
   relocate(event, .after = hr_size_ha) %>% 
-  mutate(event = ifelse(is.na(event),0,1)) %>% 
+  mutate(event = replace_na(event,0)) %>% 
   left_join(loc_timedata,
             by = c("id", "int_id")) %>% 
   relocate(c("last_day",
              "tstart",
              "tend",
              "tduration"),
-           .before = event)
+           .before = event) %>% 
+  left_join(deer_age_sex,
+            by = c("id", "int_id")) %>% 
+  relocate(c("sex",
+             "age"),
+           .after = int_id) %>% 
+  replace(is.na(.), 0) %>% 
+  mutate(year = year(last_day),.after = last_day)
 
 # Scaling Data
 for (j in ((str_which(names(loc_data_final),"event")+1):ncol(loc_data_final))) {
@@ -314,80 +334,97 @@ for (i in 1:length(cov_list_final)) {
 }
 
 ###############################################################################
-#   Model South                                                             ####
+#   Model south                                                             ####
 #      Setup                                                                ####
 
-# Home Range Data
-hr_data <- read_csv(covs_csvs[[2]]) %>% 
-  rename(individual.local.identifier = id,
-         ordered_int_id = int_id)
+# Importing Data
+hr_cov_data <- covs_csvs[2] %>% 
+  read_csv() 
 
-# Mortality Data
-mort <- mort_data %>% 
-  distinct(individual.local.identifier,ordered_int_id,t_start,t_end, .keep_all = TRUE) %>% 
-  dplyr::select(-c(location.long,location.lat,timestamp,date))
+# Identifying Max Interval 
+locs_maxintid <- hr_cov_data %>% 
+  group_by(id) %>% 
+  summarize(int_id_max = max(int_id))
 
-# Final Data
-data_final <- left_join(hr_data,mort) %>% 
-  relocate(names(mort),.after= ordered_int_id) %>% 
-  arrange(individual.local.identifier, ordered_int_id) %>% 
-  mutate_all(~replace_na(.,0)) %>% 
-  mutate(age = factor(age, levels = c("F","Y","A")),
-         sex = factor(sex, levels = c("F","M")),
-         year = as.character(year)) %>% 
-  filter(hr_type == "akde_95")
+# Creating Relevant Mortality Table
+loc_mort_table <- locs_maxintid %>%
+  mutate(event = ifelse(.$id %in% mort_table$individual.local.identifier,1,0)) %>% 
+  rename("int_id" = "int_id_max")
+
+# # Creating Final Dataset
+loc_data_final <- left_join(hr_cov_data,
+                            loc_mort_table,
+                            by = c("id",
+                                   "int_id")) %>% 
+  relocate(event, .after = hr_size_ha) %>% 
+  mutate(event = replace_na(event,0)) %>% 
+  left_join(loc_timedata,
+            by = c("id", "int_id")) %>% 
+  relocate(c("last_day",
+             "tstart",
+             "tend",
+             "tduration"),
+           .before = event) %>% 
+  left_join(deer_age_sex,
+            by = c("id", "int_id")) %>% 
+  relocate(c("sex",
+             "age"),
+           .after = int_id) %>% 
+  replace(is.na(.), 0) %>% 
+  mutate(year = year(last_day),.after = last_day)
 
 # Scaling Data
-for (j in ((str_which(names(data_final),"hr_type")+1):ncol(data_final))) {
-  data_final[,j] <- scale(data_final[,j])
+for (j in ((str_which(names(loc_data_final),"event")+1):ncol(loc_data_final))) {
+  loc_data_final[,j] <- scale(loc_data_final[,j])
   print(j)
-}
+}   
 
 #      Checking for Autocorrelation                                         ####
 
 # Initial list of covs
-cov_names_init <- names(data_final) %>% 
-  str_subset("individual.local.identifier",negate = T)  %>%
-  str_subset("month",negate = T)  %>%
+cov_names_init <- names(loc_data_final) %>% 
+  str_subset("id",negate = T)  %>%
+  str_subset("int_id",negate = T)  %>%
   str_subset("site",negate = T)  %>%
   str_subset("ordered_int_id",negate = T)  %>%
-  str_subset("t_start",negate = T)  %>%
-  str_subset("t_end",negate = T)  %>%
+  str_subset("tstart",negate = T)  %>%
+  str_subset("tend",negate = T)  %>%
   str_subset("event",negate = T)  %>%
-  str_subset("hr_type",negate = T)  %>%
+  str_subset("hr_size_ha",negate = T) %>%
+  str_subset("hr_type",negate = T) %>%
+  str_subset("last_day",negate = T) %>%
+  str_subset("tduration",negate = T) %>% 
   str_subset("Shrub",negate = T) %>% 
   str_subset("Wetland",negate = T) %>% 
   str_subset("Barren",negate = T) %>% 
   str_subset("Water",negate = T)  %>%
+  str_subset("slope",negate = T)  %>%
+  str_subset("proportion",negate = T)  %>%
+  str_subset("patchdensity_Forest",negate = T)  %>%
+  str_subset("meanpatcharea_cropland",negate = T)  %>%
   c()
 
 # Making Correlation Plot
-cov_df <- data_final[cov_names_init]
+cov_df <- loc_data_final[cov_names_init]
 
 cor_bargraph <-corr_cross(cov_df, rm.na = T, max_pvalue = 0.07, 
                           top = 40,
                           plot = T)
 
 # Final list of covs
-cov_names_final <- cov_names_init %>% 
-  str_subset("slope",negate = T) %>% 
-  str_subset("patchdensity",negate = T) %>% 
-  str_subset("aspect",negate = T) %>% 
-  str_subset("hr_size_ha",negate = T) %>% 
-  str_subset("proportion",negate = T) %>% 
-  str_subset("dem",negate = T)
+cov_names_final <- cov_names_init 
 
 #      Survival Models                                                      ####
 
 # Creating Survival Object
-surv_object <- Surv(time = data_final$t_start,
-                    time2 = data_final$t_end,
-                    event = data_final$event,
+surv_object <- Surv(time = loc_data_final$tstart,
+                    time2 = loc_data_final$tend,
+                    event = loc_data_final$event,
                     type = "counting")
 
 #        Kaplan-Meier Model                                                 ####
 #           General                                                         ####
-model_kaplanmeier_south <- survfit(surv_object ~ 1, data=data_final)
+model_kaplanmeier_south <- survfit(surv_object ~ 1, data=loc_data_final)
 
 gen_plot <-   ggsurvfit(model_kaplanmeier_south) +
   labs(
@@ -404,7 +441,7 @@ model_kaplanmeier_south %>%
   )
 
 #           Sex                                                             ####
-model_kaplanmeier_south_sex <- survfit(surv_object ~ sex, data=data_final)
+model_kaplanmeier_south_sex <- survfit(surv_object ~ sex, data=loc_data_final)
 
 sex_plot <- ggsurvfit(model_kaplanmeier_south_sex) +
   labs(
@@ -423,7 +460,7 @@ model_kaplanmeier_south_sex %>%
 
 #           Age                                                             ####
 
-model_kaplanmeier_south_age <- survfit(surv_object ~ age, data=data_final)
+model_kaplanmeier_south_age <- survfit(surv_object ~ age, data=loc_data_final)
 
 age_plot <- ggsurvfit(model_kaplanmeier_south_age) +
   labs(
@@ -449,11 +486,11 @@ formula <- as.formula(paste("surv_object ~ ",
 
 # Building Global Model
 cox_south <- coxph(formula, 
-                   data = data_final,
+                   data = loc_data_final,
                    na.action = "na.fail")
 
 # Exporting data to clusters
-clusterExport(cl=clust, varlist=c("cox_south","data_final","surv_object"), envir=environment())
+clusterExport(cl=clust, varlist=c("cox_south","loc_data_final","surv_object"), envir=environment())
 
 # Dredge
 print(paste0("Start Time:",Sys.time()))
@@ -486,11 +523,11 @@ formula <- as.formula(paste("surv_object ~ ",
 
 # Example
 coxme_model <- coxme(formula, 
-                     data = data_final,
+                     data = loc_data_final,
                      na.action = "na.fail")
 
 # Exporting data to clusters
-clusterExport(cl=clust, varlist=c("coxme_model","data_final","surv_object"), envir=environment())
+clusterExport(cl=clust, varlist=c("coxme_model","loc_data_final","surv_object"), envir=environment())
 
 # Dredge
 print(paste0("Start Time:",Sys.time()))
@@ -503,5 +540,36 @@ print(paste0("End Time:",Sys.time()))
 # Dredge Export
 write_csv(dredge_south_coxme,
           "3.Chapter2/3.Output/dredge/dredge_south_randint.csv")
+
+#          Testing for PH                                                   ####
+resids <- cox.zph(coxme_model)
+
+#        AICc Model Selection                                               ####
+
+dredge_df <- dredge(coxme_model,
+                    cluster = clust) %>% 
+  subset(delta < 2)
+
+mod_list <- get.models(dredge_df,
+                       subset = delta < 2)
+
+model_averages <- list()
+
+cov_list <- names(dredge_df)
+
+cutoff <- cov_list %>% str_which("df")-1
+
+cov_list_final <- names(dredge_df) %>% 
+  .[1:cutoff] %>% 
+  str_subset("age",negate = T) %>% 
+  str_subset("sex",negate = T)
+
+for (i in 1:length(cov_list_final)) {
+  
+  model_averages[[i]] <- modavg(mod_list,
+                                parm = cov_list_final[[i]])
+  
+  print(i)
+}
 
 ###############################################################################
